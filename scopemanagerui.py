f200f1595259892ad110b5099cefff2b75556e91
goodmanj <jcgoodman@gmail.com>
2015-07-10 17:26:13 -0400

    Scope specific UI panels, but no commands yet

--- scopemanagerui.py
+++ scopemanagerui.py
@@ -35,6 +35,36 @@ class Log(Text):
         self.see(END)
         self.update()
 
+
+class MeadePanel(Frame):
+    # Meade-specific commands
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.starlock = IntVar()
+        self.starlockCheckbox = Checkbutton (self, text='Starlock', variable = self.starlock, \
+                                          command = self.togglestarlock)
+        self.starlockCheckbox.pack(anchor=E)
+        self.safemode = IntVar()
+        Radiobutton(self, text="Safe Mode", variable=self.safemode, value=1, command=self.togglesafemode).pack(anchor=W)
+        Radiobutton(self, text="Scope Active", variable=self.safemode, value=2, command=self.togglesafemode).pack(anchor=W)
+    
+    def togglesafemode(self):
+        return
+    
+    def togglestarlock(self):
+        return
+
+class NexStarPanel(Frame):
+    # NexStar-specific commands
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.safemode = IntVar()
+        Radiobutton(self, text="Safe Mode", variable=self.safemode, value=1, command=self.togglesafemode).pack(anchor=W)
+        Radiobutton(self, text="Scope Active", variable=self.safemode, value=2, command=self.togglesafemode).pack(anchor=W)
+
+    def togglesafemode(self):
+        return
+    
 # The main window
 class ScopeManagerUI(Frame):
     def __init__(self, master=None):
@@ -156,7 +186,7 @@ class ScopeManagerUI(Frame):
         self.undosyncButton.grid(column=5,row=4)
         self.quitButton.grid(column=5,row=5)
         
-        self.messages.grid(column=1,row=6,columnspan=5)
+        self.messages.grid(column=1,row=7,columnspan=5)
 
         """serialist.Serialist() returns a list of active serial ports on this machine.
         This list is not updated while Scope Manager is running."""
@@ -164,6 +194,7 @@ class ScopeManagerUI(Frame):
         self.scopePorts = [];        
         self.scopeTypes = [];
         self.port = StringVar()
+        self.scopespecific = None;
         for port in portList:
             self.update()
             self.messages.log('Scanning '+port+' for NexStar telescopes.')
@@ -253,6 +284,8 @@ class ScopeManagerUI(Frame):
         """Handle a change in the active serial port.  Close the current port
         if it's open and attempt to open the new one."""
         if self.scope is not None and self.scope.ready:
+            if self.scopespecific is not None:
+                self.scopespecific.grid_remove()                
             if (self.scope.istracking()):
                 self.messages.log('WARNING: Tracking is still on!')
                 time.sleep(3)
@@ -261,11 +294,14 @@ class ScopeManagerUI(Frame):
         self.messages.log('Looking for '+newscopetype+' on '+str(self.port.get())+'...')       
         if newscopetype=='NexStar':
             self.scope = nexstar.NexStar(str(self.port.get()))
+            self.scopespecific = NexStarPanel(self)
         else:
             self.scope = meade.Meade(str(self.port.get()))
+            self.scopespecific = MeadePanel(self)
         if self.scope is not None and self.scope.ready:
             self.messages.log('Connected to '+newscopetype+' on '+str(self.port.get()))
             self.tracking.set(int(self.scope.istracking()))
+            self.scopespecific.grid(column=1,row=6,columnspan=5)
         else:
             self.scope.close()
             self.messages.log("Can't connect to scope on "+str(self.port.get()))
